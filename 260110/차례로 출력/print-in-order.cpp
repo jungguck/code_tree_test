423535ba2c2b0de24f4ae0f6010e9732cf2277b9
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-10 17:31:25 +0900

    Title: 차례로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/차례로 출력/print-in-order.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 13;
+    int b = 5;
+    int c;
+    for(c=0;c<13;c++){
+
+        cout<<b << " " ;
+        b = b+1;
+    }
+
+    return 0;
+}
