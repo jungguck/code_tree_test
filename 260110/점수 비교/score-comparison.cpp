223e2af311985b2b2af3bfcd3ba777da8cba8999
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-10 18:10:42 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260110/점수 비교/score-comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    if(a > c && b >=d){
+        cout << "1";
+    }
+    else if(a <= c || b <= d ){
+                cout << "0";
+    }
+
+    return 0;
+}
