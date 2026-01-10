a1367b0e67c0e26f4a01600298cd7c2c7a571ff7
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-10 21:02:33 +0900

    Title: 따옴표 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/따옴표 출력/print-quote.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string word = "He says \"It's a really simple sentence\". ";
+
+
+    cout << word  << endl;
+    return 0;
+}
