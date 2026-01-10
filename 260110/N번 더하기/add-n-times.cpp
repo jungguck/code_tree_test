9de6cc4fe6b7086bab855f0f5d8b7d95a6c2182a
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-10 17:06:50 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260110/N번 더하기/add-n-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >> a >> b;
+
+    for(c=0;c<b;c++){
+        a = a+b;
+        cout << a << endl;
+        
+    }
+
+
+    return 0;
+}
