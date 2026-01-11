54faaa78668477acba0dd1581f63cc2e53153580
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-11 13:48:27 +0900

    Title: 남녀노소 구분짓기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260111/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+
+    if(a == 0){
+        if(b >19){
+            printf("MAN");
+        }
+        else{
+            printf("BOY");
+        }
+    }
+    else if (a == 1) {
+        if(b>19){
+            printf("WOMAN");
+
+        }
+        else{
+            printf("GIRL");
+        }
+    }
+    return 0;
+}
