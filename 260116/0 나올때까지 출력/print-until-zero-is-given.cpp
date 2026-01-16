f34c2e853e4fe3a282d3458b3495de6b6afe095c
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-16 11:24:21 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260116/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int number;
+
+    while(true){
+        cin >> number;
+
+        if(number == 0){
+            
+            break;
+        }
+        cout <<number << endl;
+
+
+    }
+
+    return 0;
+}
