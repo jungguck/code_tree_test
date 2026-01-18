89444940d5123a91b877799feabc2fb5c206fd04
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-18 10:29:50 +0900

    Title: 숫자 피라미드, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    for(int i = 1 ; i<=a; i++){
+        for(int j=1; j<=i; j++){
+                cout<< i << " ";            
+        }
+        cout << "\n";
+    }
+    return 0;
+}
