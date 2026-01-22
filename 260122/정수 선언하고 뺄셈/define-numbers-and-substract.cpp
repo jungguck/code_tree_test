e97e47ee6cbe6651c30096a519b8917d520b4a93
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-22 10:12:20 +0900

    Title: 정수 선언하고 뺄셈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260122/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 97;
+    int b = 13;
+
+    int c = a-b;
+
+    cout << a << " - " << b << " = "<< c; 
+    return 0;
+}
