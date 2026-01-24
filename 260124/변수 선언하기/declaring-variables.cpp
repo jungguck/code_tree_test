1da2879ed65d2f20a577c0e3425cd2ccc1fbdc8b
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-24 19:30:52 +0900

    Title: 변수 선언하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260124/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 3;
+    char b = 'c';
+
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
