26669a131e8c8b0fa7c0b7600eafe78eed57a703
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-25 19:49:01 +0900

    Title: 변수 출력하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260125/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 3;
+    char b = 'c';
+
+    cout << a << "..."<< b ;
+    // Please write your code here.
+    return 0;
+}
