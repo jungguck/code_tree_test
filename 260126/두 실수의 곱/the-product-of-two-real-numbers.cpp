80927d2bf01b7df06ea30ecc6667832e4ae82914
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-26 13:42:45 +0900

    Title: 두 실수의 곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 5.26;
+    double b = 8.27;
+    double c = a*b;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << c;
+    return 0;
+}
