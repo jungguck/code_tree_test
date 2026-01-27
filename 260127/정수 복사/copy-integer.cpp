f9e81e7a5769e79965b1af2f8bbad98a47b2d432
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-27 12:48:58 +0900

    Title: 정수 복사, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260127/정수 복사/copy-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 4;
+    b = a;
+
+    int c = a*b;
+
+    cout << a << " " << b << endl
+    cout <<c;
+    return 0;
+}
