0f5d0fca677675cebaa0bc553f8b133ea16898cb
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-28 11:07:22 +0900

    Title: 변수 값 교체하기 6, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 2;
+    int b= 5;
+    int c = a;
+
+    a= b;
+    b= c;
+
+    cout << a << endl;
+    cout << b;
+
+    return 0;
+}
