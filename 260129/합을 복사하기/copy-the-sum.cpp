7cbeb067935a7f9f1b9249bcf496e32579c29e0b
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-29 15:43:01 +0900

    Title: 합을 복사하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a =1;
+    int b =2;
+    int c = 3;
+    int total = a+b+c;
+
+    a=b=c= total;
+
+    cout << a << " " << b<< " " << c;
+
+
+    return 0;
+}
