ca2c4caa2ca391aed060b9e1554acf72e1056ff3
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-29 15:34:37 +0900

    Title: 변수 값 복사하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp =c;
+
+    b=a=c;
+    cout << a << " " << b << " " << c;
+    return 0;
+}
