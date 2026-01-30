43784b431f2b5d646946caeaa5b8fcecac6c0b8f
jungguck <142872340+jungguck@users.noreply.github.com>
2026-01-30 14:55:23 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260130/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int number;
+    cin >> number;
+
+    cout << "Your score is " << number << " point.";
+    return 0;
+}
