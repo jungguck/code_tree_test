1d64cde849587631bc4b4601ad53d638f58f314c
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:35:25 +0900

    Title: 입력받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/입력받아 출력/input-and-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    cout << a << " " << b;
+    return 0;
+}
