078c8380be066bd2460a06bed20324c3ae27706a
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:49:41 +0900

    Title: 정수 세 개 입력받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b >> c;
+
+    cout << a << " " << b << " " << c;
+    return 0;
+}
