f990f3fa527d7e2dd18590a85e77e8309bbdcdb1
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 13:56:15 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    float a;
+
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a<<endl;
+
+    return 0;
+}
