eb6743bafdbce9c0ea070944cd1d8e4b9f5fe983
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 13:58:41 +0900

    Title: 길이 단위 환산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(1);
+
+    float b;
+    b = a*30.48;
+
+    cout << b;
+    
+
+    return 0;
+}
