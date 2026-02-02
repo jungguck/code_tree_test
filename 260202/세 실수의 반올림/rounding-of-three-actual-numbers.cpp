34f599e4b00bc28e6f834f899d4b7cab351a0202
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:39:49 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+    float b;
+    float c;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << endl;
+    cout << b << endl;    
+    cout << c << endl;
+
+    return 0;
+}
