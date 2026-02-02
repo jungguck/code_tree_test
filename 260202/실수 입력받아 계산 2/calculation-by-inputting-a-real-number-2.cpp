6603e7ab319c76eb0803b2b055c6a3bed53d08c0
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:04:16 +0900

    Title: 실수 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+
+    cin >> a;
+    
+    a = a + 1.5;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a;
+    
+    return 0;
+}
