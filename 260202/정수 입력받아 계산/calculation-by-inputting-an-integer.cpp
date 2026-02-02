25eb496f8aa8a4b146a595f870aa2a77ff280346
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 13:52:17 +0900

    Title: 정수 입력받아 계산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    int b = a*2;
+    b = b+3;
+
+    cout << b;
+    return 0;
+}
