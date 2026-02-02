fe3ba8ff5a92bff595d1b8c97d50ca8fd8e74389
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:26:06 +0900

    Title: 입력받은 값과 합 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b;
+    c = a+b;
+
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
