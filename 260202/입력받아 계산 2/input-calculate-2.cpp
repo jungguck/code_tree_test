63563813ef81b5a11d83fcd7a206ef9ae93902d3
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-02 14:18:17 +0900

    Title: 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/입력받아 계산 2/input-calculate-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b; 
+    cin >> a;
+    cin >> b;
+    int c = a*b;
+    cout << c;
+    return 0;
+}
