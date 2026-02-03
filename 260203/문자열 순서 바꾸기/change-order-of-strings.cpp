20749d47ee63481c259f8eb88cc564c4e43c957e
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-03 11:53:12 +0900

    Title: 문자열 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string S;
+    string T;
+
+    cin >> S;
+    cin >> T;
+
+
+    cout << T << endl;
+    cout << S << endl;
+    return 0;
+}
