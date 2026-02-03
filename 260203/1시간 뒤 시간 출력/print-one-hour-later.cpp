78036e966aeb517e5355e29adf3a4a276062d481
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-03 11:58:39 +0900

    Title: 1시간 뒤 시간 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int h;
+    int m;
+
+    int a = 1;
+
+    cin >> h;
+    cin.get();
+    cin >> m;
+
+    a = a+h;
+    cout << a << ":" <<m;
+
+
+    return 0;
+}
