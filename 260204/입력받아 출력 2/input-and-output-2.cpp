0df2e73dc68fa49783b6aba9ec44bcc8c538fa39
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-04 11:33:07 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260204/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a,b,c,d,e,f,g,h,i,j,k,o,l;
+    char z;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+    cin.get();
+    cin >> g;
+    cin >> h;
+    cin >> i;
+    cin >> j;
+    cin >> k;
+    cin >> l;
+    cin >> o;
+
+
+    cout << a << b << c << d << e << f << g << h << i << j << k  << l << o; 
+
+
+    
+
+    return 0;
+}
