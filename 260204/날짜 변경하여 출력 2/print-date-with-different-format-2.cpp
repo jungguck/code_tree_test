c6328da0eea7505aac0581e3215f7d3a221d5f6a
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-04 11:14:04 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260204/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+   
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << c << "."<< b << "." << a;
+
+    
+
+    return 0;
+}
