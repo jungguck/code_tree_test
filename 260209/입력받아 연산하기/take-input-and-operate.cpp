7e76d3cd3b7b9ae3a4782ae9c50b8e17a33ada02
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-09 13:35:24 +0900

    Title: 입력받아 연산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260209/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+
+    cin >> a >> b;
+    
+    int c = a+87;
+    int d = b%10;
+
+    cout << c << endl;
+    cout << d << endl;
+
+    return 0;
+}
