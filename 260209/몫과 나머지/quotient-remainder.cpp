c097fff90995a641ba24375878308191b278395b
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-09 12:32:47 +0900

    Title: 몫과 나머지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260209/몫과 나머지/quotient-remainder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin >> a >> b;
+
+    int c = a/b;
+    int d = a%b;
+
+    cout << c << "..." << d;
+    return 0;
+}
