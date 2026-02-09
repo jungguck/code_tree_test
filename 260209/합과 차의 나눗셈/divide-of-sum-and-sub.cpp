4a9c79dea8be6716af0c6fb0d4ecd282b9093d2b
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-09 14:11:37 +0900

    Title: 합과 차의 나눗셈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260209/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a , b;
+    
+    cin >> a >> b;
+
+    double c = a+b;
+    double d = a-b;
+
+    cout<<fixed;
+    cout.precision(2);
+    cout << c/d ;
+    return 0;
+}
