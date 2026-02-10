bf8718eae2a60af137b148ed47804602cfd237f6
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-10 10:27:08 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260210/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+
+    cin >> a >> b >> c;
+    int d = a+b+c;
+    int e = (a+b+c)/3;
+
+    cout << d << endl;
+    cout << e << endl;
+    cout << d-e << endl;
+
+
+    return 0;
+}
