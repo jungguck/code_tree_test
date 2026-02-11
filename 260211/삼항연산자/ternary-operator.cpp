7ace4edd695123fff183bb0b00facde4cc1f3d77
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-11 11:36:34 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260211/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+
+    cin >> a;
+
+    if(a == 100){
+        cout << "pass";
+    }
+    else{
+        cout << "failure";
+    }
+
+    return 0;
+}
