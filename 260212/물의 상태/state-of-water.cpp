5da501999ea24276e7fc2dedecaaf5ecf7025067
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-12 09:50:10 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260212/물의 상태/state-of-water.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+
+    cin >> a;
+
+    if (a>=100){
+        cout << "vapor";
+    }
+    else if (a>0){
+        cout << "water";
+    }
+    else {
+        cout << "ice";
+    }
+    return 0;
+}
