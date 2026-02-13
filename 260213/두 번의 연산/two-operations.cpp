13feeff0c08d5eaebc88e97c70e80b6a802df2c6
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-13 10:55:12 +0900

    Title: 두 번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260213/두 번의 연산/two-operations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    if (a%2== 1){
+        a = a+3;
+    }
+    if (a%3 == 0){
+        a = a/3;
+    }
+
+    cout << a;
+
+    return 0;
+}
