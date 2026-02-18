4b3c44db3357150792fb208abc3e96a48dc99e1a
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-18 10:59:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260218/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >> a >> b >>c;
+
+    if(b>=a && c>=a ){
+        cout << "1 ";
+    }
+    else{
+        cout << "0 ";
+    }
+
+    if(b == a  && a==c ){
+        cout << "1 ";
+    }
+    else{
+        cout << "0 ";
+    }
+
+
+    return 0;
+}
