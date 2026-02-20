6dfc931a15eb26de01a8374b3561c35793d6edad
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-20 10:06:03 +0900

    Title: 장학금, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260220/장학금/scholarship.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a ,b;
+    cin >> a >>b;
+
+    if(a>=90 && b>=95){
+        cout << "100000";
+    }
+    else if (a>=90  && b>=90){
+        cout << "50000";
+    }
+    else{
+        cout <<"0";
+    }
+
+    return 0;
+}
