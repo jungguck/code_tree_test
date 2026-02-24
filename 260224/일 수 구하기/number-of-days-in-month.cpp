372d8a9de935c5260bc1c0de47a845fa8381539e
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-24 09:53:28 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260224/일 수 구하기/number-of-days-in-month.cpp
+++ 260224/일 수 구하기/number-of-days-in-month.cpp
@@ -6,16 +6,15 @@ int main() {
     int a;
     cin >> a;
 
-    if (a%2 == 1 || a == 9){
+    if (a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10|| a == 12 ){
         cout << "31";
     }
-    else if (a%2 == 0){
-        if (a == 2 ){
-            cout <<"28";
-        }
-        else{
+    else if (a == 4 || a == 6 || a == 9 || a == 11 ){
         cout <<"30";
-        }
     }
+    else if (a == 2 ){
+        cout <<"28";
+    }
+
     return 0;
 }
