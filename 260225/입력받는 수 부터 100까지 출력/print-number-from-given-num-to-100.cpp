b6849c678e83d6c967b0c2f2798240e5599fce11
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-25 21:16:20 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260225/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >>a;
+
+    for (int i=a ; i<=100;i++){
+        cout << a << " ";
+        a = a+1;
+    }
+    return 0;
+}
