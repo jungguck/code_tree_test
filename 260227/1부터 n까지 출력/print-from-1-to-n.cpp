b3322e77401b6aee1fc3a4f1730526c876e49b12
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-27 09:53:05 +0900

    Title: 1부터 n까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260227/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    for(int i = 1 ; i <= a; i ++){
+        cout << i << " " ;
+    }
+
+    return 0;
+}
