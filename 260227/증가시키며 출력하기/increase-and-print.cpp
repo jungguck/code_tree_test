26e7479b509abff70cf26b9696ff80f8f39dfa32
jungguck <142872340+jungguck@users.noreply.github.com>
2026-02-27 09:56:01 +0900

    Title: 증가시키며 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260227/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 18;
+    for(int i = 5 ; i<18 ; i += 2){
+        cout << i << " ";
+    } 
+
+    return 0;
+}
