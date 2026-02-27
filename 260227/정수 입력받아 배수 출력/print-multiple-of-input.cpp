0c8370b086346ec1209cc2d24749eb53ba7e8168
Tuna_raccon_jk <142872340+jungguck@users.noreply.github.com>
2026-02-27 10:20:50 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260227/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,c;
+    cin >> a;
+    c = a;
+    for (int i = 1; i<=5; i++){
+        c = a*i;
+        cout << c << " ";
+        
+    }
+
+    return 0;
+}
