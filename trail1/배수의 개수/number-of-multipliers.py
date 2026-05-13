028a5e58d6c5ed45fd02d4c87cc65f4ef3f40751
Tuna_raccon_jk <142872340+jungguck@users.noreply.github.com>
2026-05-13 21:16:01 +0900

    [Passed] 배수의 개수 (45ms, 16MB)

---
+++ trail1/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,17 @@
+cnt_3 = 0
+cnt_5 = 0
+
+
+for _ in range(10):
+    n = int(input())
+
+    if n % 3 == 0:
+
+        cnt_3 += 1
+
+    if n % 5 == 0:
+
+        cnt_5 += 1
+
+print(cnt_3 , end = " ")
+print(cnt_5 , end = " ")
